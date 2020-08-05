4a1c0ed587929d203e71bb7f06f81569616b267e
skizhu <69230144+skizhu@users.noreply.github.com>
2020-08-05 00:40:13 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 var clip1 = new Audio('se2.mp3');
-var clip2 = new Audio('se3.mp3');
-var clip3 = new Audio('se4.mp3');
-var clip4 = new Audio('se5.mp3');
-var clip5 = new Audio('se6.mp3');
-var clip6 = new Audio('se7.mp3');
 var special = new Audio('special.mp3');
 
 var clips = [clip1, special];
