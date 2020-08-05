0b7920abff72c6a32c3bed9b600de9c260536869
skizhu <69230144+skizhu@users.noreply.github.com>
2020-08-05 00:34:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 var clip1 = new Audio('se2.mp3');
+var clip2 = new Audio('se3.mp3');
+var clip3 = new Audio('se4.mp3');
+var clip4 = new Audio('se5.mp3');
+var clip5 = new Audio('se6.mp3');
+var clip6 = new Audio('se7.mp3');
 var special = new Audio('special.mp3');
 
 var clips = [clip1, special];
@@ -6,7 +11,7 @@ var clips = [clip1, special];
 clips.volume = 0.2;
 
 function nice_function(){
-    var shuffledClip = Math.floor(Math.random() * 2);
+    var shuffledClip = Math.floor(Math.random() * 7);
     var randomClip = clips[shuffledClip];
     console.log(randomClip)
     randomClip.play();
